39398ec1c29d52ec3fd482e1edf74fe292c3bd57
Milenko Castillo <milenko.castillo@gmail.com>
2018-04-16 00:48:29 -0300

    orimer commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :logged_filter, except: [:index]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  private
+
+  def filter_admin!
+    authenticate_user!
+    redirect_to root_path, alert: "No tienes acceso" unless current_user.admin?
+  end
+
+  def logged_filter
+    redirect_to root_path, alert: "No tienes acceso" unless user_signed_in?
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703175956) do
+ActiveRecord::Schema.define(version: 20180223193511) do
 
   create_table "histories", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,28 @@ ActiveRecord::Schema.define(version: 20170703175956) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_histories_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
+    t.boolean "admin"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
